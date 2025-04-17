316a429907811ed107f87fc95d89dbd6e8dd7343
Vladislav Opolev <v.opolev@itsv1eds.ru>
2025-04-17 22:17:23 +0500

    [update] v5.0
    update the information, optimize the code, also delete unnecessary and old files

--- js/index.js
+++ js/index.js
@@ -1,192 +1,120 @@
-$(window).on('load',function(){
+// TxtRotate класс (вынесено из $(function(){ ... }))
+function TxtRotate(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.isDeleting = false;
+  this.tick();
+}
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+  this.txt = this.isDeleting ? fullTxt.substring(0, this.txt.length - 1) : fullTxt.substring(0, this.txt.length + 1);
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+  if (this.isDeleting) delta /= 2;
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 100;
+  }
+  setTimeout(function() { that.tick(); }, delta);
+};
+
+console.log('TxtRotate loaded!');
+
+$(function(){
+  // Loader animation
+  // Loader animation (запускаем сразу после готовности DOM)
+  console.log('DOM ready - запускаем анимацию loader');
   gsap.to('#loader',1,{y:"-100%"});
   gsap.to('#loader',1,{opacity:0});
   gsap.to('#loader',0,{display:"none",delay:1});
-  gsap.to('#header',0,{display:"block",delay:1})
+  gsap.to('#header',0,{display:"block",delay:1});
   gsap.to('#navigation-content',0,{display:"none"});
   gsap.to('#navigation-content',0,{display:"flex",delay:1});
-})
-$(function(){
+
+  // TxtRotate
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0em solid #666 ; }";
+  document.body.appendChild(css);
+
+  // Цветовая панель
   $('.color-panel').on("click",function(e) {
     e.preventDefault();
     $('.color-changer').toggleClass('color-changer-active');
-});
-$('.colors a').on("click",function(e) {
-  e.preventDefault();
-  var attr = $(this).attr("title");
-  console.log(attr);
-  $('head').append('<link rel="stylesheet" href="css/'+attr+'.css">');
-});
-});
-$(function(){
-     $('.menubar').on('click',function(){
-         gsap.to('#navigation-content',.6,{y:0});
-     })
-     $('.navigation-close').on('click',function(){
-        gsap.to('#navigation-content',.6,{y:"-100%"});
-    });
-   }); 
+  });
+  $('.colors a').on("click",function(e) {
+    e.preventDefault();
+    var attr = $(this).attr("title");
+    $('head').append('<link rel="stylesheet" href="css/'+attr+'.css">');
+  });
 
-$(function(){
-    var TxtRotate = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-      };
-      
-      TxtRotate.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-      
-        if (this.isDeleting) {
-          this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-          this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-      
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-      
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-      
-        if (this.isDeleting) { delta /= 2; }
-      
-        if (!this.isDeleting && this.txt === fullTxt) {
-          delta = this.period;
-          this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-          this.isDeleting = false;
-          this.loopNum++;
-          delta = 100;
-        }
-      
-        setTimeout(function() {
-          that.tick();
-        }, delta);
-      };
-      
-      window.onload = function() {
-        var elements = document.getElementsByClassName('txt-rotate');
-        for (var i=0; i<elements.length; i++) {
-          var toRotate = elements[i].getAttribute('data-rotate');
-          var period = elements[i].getAttribute('data-period');
-          if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
-          }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".txt-rotate > .wrap { border-right: 0em solid #666 ; }";
-        document.body.appendChild(css);
-      };
-})
-$(function(){
+  // Меню-бургер
+  $('.menubar').on('click',function(){
+    gsap.to('#navigation-content',.6,{y:0});
+  });
+  $('.navigation-close').on('click',function(){
+    gsap.to('#navigation-content',.6,{y:"-100%"});
+  });
 
-    $('#about-link').on('click',function(){
-      gsap.to('#navigation-content',0,{display:"none",delay:.7});
-      gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-  gsap.to('#header',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-   gsap.to('#breaker',0,{display:"block"});
-   gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#contact',0,{display:"none"});
-   gsap.to('#breaker',0,{display:"none",delay:2});
-   gsap.to('#breaker-two',0,{display:"none",delay:2});
-   gsap.to('#about',0,{display:"block",delay:.7});
-   gsap.to('#navigation-content',0,{display:'flex',delay:2});
- })
- $('#contact-link').on('click',function(){
-   gsap.to('#navigation-content',0,{display:"none",delay:.7});
-   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-gsap.to('#breaker',0,{display:"block"});
-gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#breaker',0,{display:"none",delay:2});
-gsap.to('#breaker-two',0,{display:"none",delay:2});
-gsap.to('#contact',0,{display:"block",delay:.7});
-gsap.to('#navigation-content',0,{display:'flex',delay:2});
-})
-$('#portfolio-link').on('click',function(){
-  gsap.to('#navigation-content',0,{display:"none",delay:.7});
-  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#contact',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#breaker',0,{display:"block"});
-gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#breaker',0,{display:"none",delay:2});
-gsap.to('#breaker-two',0,{display:"none",delay:2});
-gsap.to('#portfolio',0,{display:"block",delay:.7});
-gsap.to('#navigation-content',0,{display:'flex',delay:2});
-})
-$('#blog-link').on('click',function(){
-  gsap.to('#navigation-content',0,{display:"none",delay:.7});
-  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-gsap.to('#contact',0,{display:"none"});
-gsap.to('#breaker',0,{display:"block"});
-gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#breaker',0,{display:"none",delay:2});
-gsap.to('#breaker-two',0,{display:"none",delay:2});
-gsap.to('#blog',0,{display:"block",delay:.7});
-gsap.to('#navigation-content',0,{display:'flex',delay:2});
-})
-$('#home-link').on('click',function(){
-  gsap.to('#navigation-content',0,{display:"none",delay:.7});
-  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-gsap.to('#contact',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#breaker',0,{display:"block"});
-gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#breaker',0,{display:"none",delay:2});
-gsap.to('#breaker-two',0,{display:"none",delay:2});
-gsap.to('#header',0,{display:"block",delay:.7});
-gsap.to('#navigation-content',0,{display:'flex',delay:2});
-})
-
-})
-$(function(){
- var body =  document.querySelector('body');
- var $cursor = $('.cursor')
-   function cursormover(e){
-    
-    gsap.to( $cursor, {
-      x : e.clientX ,
-      y : e.clientY,
-      stagger:.002
-     })
-   }
-   function cursorhover(e){
-    gsap.to( $cursor,{
-     scale:1.4,
-     opacity:1
-    })
-    
+  // Универсальное переключение секций
+  function showSection(id){
+    var all=['header','about','portfolio','blog','contact'];
+    all.forEach(function(s){$('#'+s).hide();});
+    if(id) $('#'+id).show();
   }
-  function cursor(e){
-    gsap.to( $cursor, {
-     scale:1,
-     opacity:.6
-    }) 
-  }
-  $(window).on('mousemove',cursormover);
-  $('.menubar').hover(cursorhover,cursor);
-  $('a').hover(cursorhover,cursor);
-  $('.navigation-close').hover(cursorhover,cursor);
+  $('#home-link').on('click',function(e){e.preventDefault();showSection('header');});
+  $('#portfolio-link').on('click',function(e){e.preventDefault();showSection('portfolio');});
+  $('#about-link').on('click',function(e){e.preventDefault();showSection('about');});
+  $('#blog-link').on('click',function(e){e.preventDefault();showSection('blog');});
+  $('#contact-link').on('click',function(e){e.preventDefault();showSection('contact');});
 
-})
+  // Кастомный курсор
+  var $cursor = $('.cursor');
+  $(window).on('mousemove',function(e){
+    gsap.to($cursor,{x:e.clientX,y:e.clientY,stagger:.002});
+  });
+  $('.menubar, a, .navigation-close').hover(
+    function(){gsap.to($cursor,{scale:1.4,opacity:1});},
+    function(){gsap.to($cursor,{scale:1,opacity:.6});}
+  );
+
+  // Универсальное переключение секций
+  function showSection(id){
+    var all=['header','about','portfolio','blog','contact'];
+    all.forEach(function(s){$('#'+s).hide();});
+    if(id) $('#'+id).show();
+  }
+  $('#home-link').on('click',function(e){e.preventDefault();showSection('header');});
+  $('#portfolio-link').on('click',function(e){e.preventDefault();showSection('portfolio');});
+  $('#about-link').on('click',function(e){e.preventDefault();showSection('about');});
+  $('#blog-link').on('click',function(e){e.preventDefault();showSection('blog');});
+  $('#contact-link').on('click',function(e){e.preventDefault();showSection('contact');});
+
+  // Кастомный курсор
+  var $cursor = $('.cursor');
+  $(window).on('mousemove',function(e){
+    gsap.to($cursor,{x:e.clientX,y:e.clientY,stagger:.002});
+  });
+  $('.menubar, a, .navigation-close').hover(
+    function(){gsap.to($cursor,{scale:1.4,opacity:1});},
+    function(){gsap.to($cursor,{scale:1,opacity:.6});}
+  );
+
+});
